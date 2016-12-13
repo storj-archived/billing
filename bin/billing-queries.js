cabfb6587862c993ae1bf647ee5908e6dc99d2a0
Bryan White <bryanchriswhite@gmail.com>
2016-12-13 23:16:04 +0100

    env var changes

--- bin/billing-queries.js
+++ bin/billing-queries.js
@@ -4,29 +4,36 @@ console.log('HELLO FROM BILLING-QUERIES!');
 
 const moment = require('moment');
 const Storage = require('storj-service-storage-models');
-const storage = new Storage(process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/__storj-billing-development', process.env.MONGO_OPTIONS || {});
+
+const mongoOptions = process.env.MONGO_OPTIONS || {};
+mongoOptions.auth = {
+  user: process.env.MONGO_USERNAME || '',
+  pass: process.env.MONGO_PASSWORD || ''
+};
+
+const storage = new Storage(process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/__storj-billing-development', mongoOptions);
 const User = storage.models.User;
 
 const CronJob = require('cron').CronJob;
 const job = new CronJob({
   cronTime: '0 0 0 * * *',
   onTick: function() {
-    console.log('CronJob Starting - Billing Counts')
+    console.log('CronJob Starting - Billing Counts');
     User
-      .find({})
-      .then(function(users) {
-        users.forEach(function(user) {
-          const now = moment().utc;
-          const endTimestamp = moment.utc(
-            `${now.year()}-${now.month()}-${now.date()}`,
-            'YYYY-MM-DD'
-          );
-          const beginTimestamp = moment.utc(endTimestamp).subtract(1, 'day');
+        .find({})
+        .then(function(users) {
+          users.forEach(function(user) {
+            const now = moment().utc;
+            const endTimestamp = moment.utc(
+                `${now.year()}-${now.month()}-${now.date()}`,
+                'YYYY-MM-DD'
+            );
+            const beginTimestamp = moment.utc(endTimestamp).subtract(1, 'day');
 
-          user.generateDebitsForStorage(beginTimestamp, endTimestamp);
-          user.generateDebitsForBandwidth(beginTimestamp, endTimestamp);
-        })
-      });
+            user.generateDebitsForStorage(beginTimestamp, endTimestamp);
+            user.generateDebitsForBandwidth(beginTimestamp, endTimestamp);
+          })
+        });
   },
   start: false,
   timeZone: 'UTC'
