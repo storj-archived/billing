0d5b156782db9b87570cf8abbb788665a721f9dc
Bryan White <bryanchriswhite@gmail.com>
2016-12-20 23:43:59 +0100

    billing importer working

--- bin/billing-queries.js
+++ bin/billing-queries.js
@@ -5,37 +5,47 @@ console.log('HELLO FROM BILLING-QUERIES!');
 const moment = require('moment');
 const Storage = require('storj-service-storage-models');
 
-const mongoOptions = process.env.MONGO_OPTIONS || {};
+const mongoOptions = JSON.parse(process.env.MONGO_OPTIONS || {});
 mongoOptions.auth = {
   user: process.env.MONGO_USERNAME || '',
   pass: process.env.MONGO_PASSWORD || ''
 };
 
 const storage = new Storage(process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/__storj-billing-development', mongoOptions);
-const User = storage.models.User;
+const generateDebits = require('../lib/queries/generate-debits')(storage);
 
-const CronJob = require('cron').CronJob;
-const job = new CronJob({
-  cronTime: '0 0 0 * * *',
-  onTick: function() {
-    console.log('CronJob Starting - Billing Counts');
-    User
-        .find({})
-        .then(function(users) {
-          users.forEach(function(user) {
-            const now = moment().utc;
-            const endTimestamp = moment.utc(
-                `${now.year()}-${now.month()}-${now.date()}`,
-                'YYYY-MM-DD'
-            );
-            const beginTimestamp = moment.utc(endTimestamp).subtract(1, 'day');
+storage.connectedPromise
+    .then(function() {
+      const CronJob = require('cron').CronJob;
+      const job = new CronJob({
+        cronTime: '0 0 0 * * *',
+        onTick: function() {
+          console.log('connected!');
+          const now = moment().utc();
+          const endTimestamp = moment.utc(
+              `${now.year()}-${now.month() + 1}-${now.date()}`,
+              'YYYY-MM-DD'
+          ).valueOf();
+          const beginTimestamp = moment.utc(endTimestamp).subtract(1, 'day').valueOf();
+          console.log(`timestamp range: ${beginTimestamp}-${endTimestamp}`);
 
-            user.generateDebitsForStorage(beginTimestamp, endTimestamp);
-            user.generateDebitsForBandwidth(beginTimestamp, endTimestamp);
-          })
-        });
-  },
-  start: false,
-  timeZone: 'UTC'
-});
-job.start();
+          console.log('starting...');
+          const bandwidthDebitsPromise = generateDebits
+              .forBandwidth(beginTimestamp, endTimestamp, CENTS_PER_GB_BANDWIDTH)
+              .then(() => console.log('... forBandwidth done!'));
+          const storageDebitsPromise = generateDebits
+              .forStorage(beginTimestamp, endTimestamp, CENTS_PER_GB_STORAGE)
+              .then(() => console.log('... forStorage done!'));
+
+          Promise.all([bandwidthDebitsPromise, storageDebitsPromise])
+              .then(() => process.exit(0));
+        },
+        start: false,
+        timeZone: 'UTC'
+      });
+      job.start();
+    })
+    .catch(function(err) {
+      console.error(err);
+      process.exit(1);
+    });
