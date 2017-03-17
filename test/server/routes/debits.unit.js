42678778fa7f37689879bb45a36fe96bc4b63900
Dylan Lott <dylan@storj.io>
2017-03-17 14:34:11 -0600

    WIP tests for billing

--- test/server/routes/debits.unit.js
+++ test/server/routes/debits.unit.js
@@ -1,5 +1,6 @@
 const httpMocks = require('node-mocks-http');
 const sinon = require('sinon');
+require('sinon-as-promised');
 const storj = require('storj-lib');
 const expect = require('chai').expect;
 const EventEmitter = require('events').EventEmitter;
@@ -9,23 +10,109 @@ const ReadableStream = require('stream').Readable;
 const errors = require('storj-service-error-types');
 const routerOpts = require('../../_fixtures/router-opts');
 
-describe('Debits Router', function() {
+let sandbox;
 
+beforeEach(function() {
+  sandbox = sinon.sandbox.create();
+});
+
+afterEach(function() {
+  sandbox.restore();
+});
+
+describe('#debitsRouter', function() {
   const debitsRouter = new DebitsRouter(routerOpts);
 
-  describe('smoke test', function()  {
+  describe('smoke test', function() {
+    it('debitsRouter should exist with proper configs', function(done) {
+      expect(debitsRouter).to.be.ok;
+      expect(debitsRouter.storage).to.be.ok;
+      expect(debitsRouter.models).to.be.ok;
+      expect(debitsRouter.mailer).to.be.ok;
+      expect(debitsRouter.contracts).to.be.ok;
+      expect(debitsRouter.config).to.equal(routerOpts.config);
+      done();
+    });
+  });
 
-      it('debitsRouter should exist with proper configs', function(done) {
-        expect(debitsRouter).to.be.ok;
-        expect(debitsRouter.storage).to.be.ok;
-        expect(debitsRouter.models).to.be.ok;
-        expect(debitsRouter.mailer).to.be.ok;
-        expect(debitsRouter.contracts).to.be.ok;
-        expect(debitsRouter.config).to.equal(routerOpts.config);
-        done();
+  describe('#createDebit', function() {
+
+
+    it('should create a bucket and return a bucket object', function(done) {
+      // var mockUser = new debitsRouter.storage.models.User({
+      //   _id: 'dylan@storj.io',
+      //   hashpass: storj.utils.sha256('password')
+      // });
+      //
+      // var mockDebit = new debitsRouter.storage.models.Debit({
+      //   amount: 666,
+      //   user: 'lott.dylan@gmail.com',
+      //   type: 'bandwidth',
+      //   created: new Date()
+      // });
+
+      var req = httpMocks.createRequest({
+        method: 'POST',
+        url: '/debits',
+        body: {
+          user: "user@example.com",
+          type: "bandwidth",
+          amount: 123456,
+          created: new Date()
+        }
       });
 
+      var res = httpMocks.createResponse({
+        eventEmitter: EventEmitter,
+        req: req
+      });
+      // req.user = mockUser;
 
+      // var _createDebit = sandbox
+      //   .stub(debitsRouter.storage.models.Debit, 'create')
+        // .resolves(mockDebit)()
+        // .then((data) => {
+        //   console.log(data);
+        // })
+
+      res.on('send', function() {
+        console.log('res on send: ', res._getData());
+      });
+
+      res.on('end', function() {
+        console.log('res on data', res._getData());
+      });
+
+      debitsRouter.createDebit(req, res);
+      // console.log(res._getData());
+      done();
+    });
+
+    // it('should return 500 error code', function(done) {
+    //   var req = httpMocks.createRequest({
+    //     method: 'POST',
+    //     url: '/debits'
+    //   });
+    //
+    //   var res = httpMocks.createResponse({
+    //     eventEmitter: EventEmitter,
+    //     req: req
+    //   });
+    //
+    //   sandbox.stub(debitsRouter.storage.models.Debit, 'create')
+    //     .withArgs({
+    //       user: "user@example.com",
+    //       type: "bandwidth",
+    //       amount: 123456,
+    //       created: new Date()
+    //     })
+    //
+    //   debitsRouter.createDebit(req, res);
+    //
+    //   expect(res._getData()).to.equal(500);
+    //   expect(res.body.error).to.be.ok;
+    //   done();
+    // });
 
   });
 
