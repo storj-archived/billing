6a1a9d51e427f9a72f54e90937c2dc5eb42ec48a
Dylan Lott <dylan@storj.io>
2017-03-21 15:21:06 -0600

    Sinon Promise tests working

--- test/server/routes/credits.unit.js
+++ test/server/routes/credits.unit.js
@@ -1,5 +1,7 @@
 const httpMocks = require('node-mocks-http');
 const sinon = require('sinon');
+const stubPromise = require('sinon-stub-promise');
+stubPromise(sinon);
 const storj = require('storj-lib');
 const expect = require('chai').expect;
 const EventEmitter = require('events').EventEmitter;
@@ -8,8 +10,10 @@ const errors = require('storj-service-error-types');
 // const log = require('../../../lib/logger');
 const routerOpts = require('../../_fixtures/router-opts');
 const mongoose = require('mongoose');
+const constants = require('../../../lib/constants');
 
 describe('Credits Router', function() {
+  console.log('### NODE_ENV: ', process.env.NODE_ENV);
   const creditsRouter = new CreditsRouter(routerOpts);
   let sandbox;
 
@@ -30,7 +34,9 @@ describe('Credits Router', function() {
   });
 
   afterEach(function() {
-    // how do afterEach apply to it / describe blocks ??
+    creditsRouter.models.Marketing.find({}).remove();
+    creditsRouter.models.Credit.find({}).remove();
+    creditsRouter.models.Referral.find({}).remove();
     sandbox.restore();
   });
 
@@ -38,9 +44,37 @@ describe('Credits Router', function() {
 
     describe('#handleReferralSignups', function() {
 
-      it('return create referral with valid props', function() {
-        sandbox.stub();
-        var request = httpMocks.createRequest({
+      it('create referral with valid props', function(done) {
+        const mockMarketingDoc = new creditsRouter.models.Marketing({
+          user: 'lott.dylan@gmail.com',
+          created: Date.now(),
+          referralLink: 'abc-123'
+        });
+
+        sandbox.stub(creditsRouter.models.Marketing, 'create')
+          .callsArg(1);
+
+        const mockCredit = new creditsRouter.models.Credit({
+          invoiced_amount: 1234,
+          user: 'lott.dylan@gmail.com'
+        });
+
+        const mockReferral = new creditsRouter.models.Referral({
+          sender: {
+            email: 'sender@example.com',
+            amount_to_credit: 1234,
+            credit: '',
+            referralLink: 'abc-123'
+          },
+          recipient: {
+            email: 'recipient@example.com',
+            amount_to_credit: 1234,
+          },
+          created: Date.now(),
+          count: 1
+        })
+
+        var req = httpMocks.createRequest({
           method: 'POST',
           url: '/signups',
           body: {
@@ -49,22 +83,42 @@ describe('Credits Router', function() {
           }
         });
 
-        var response = httpMocks.createResponse({
-          req: request,
+        var res = httpMocks.createResponse({
+          req: req,
           eventEmitter: EventEmitter
         });
 
-        response.on('end', function() {
-          console.log('this doesnt fire');
+        var isValidPromise = sandbox.stub(creditsRouter.models.Marketing, 'isValidReferralLink')
+          .returnsPromise();
+        isValidPromise.resolves(mockMarketingDoc);
+
+        var getReferralPromise = sandbox.stub(creditsRouter, '_getReferral')
+          .returnsPromise();
+        getReferralPromise.resolves(req.body, mockMarketingDoc);
+
+        var issueReferralPromise = sandbox.stub(creditsRouter, '_issueReferralSignupCredit')
+          .returnsPromise();
+        issueReferralPromise.resolves(mockCredit);
+
+        var convertReferral = sandbox.stub(creditsRouter, '_convertReferralRecipient')
+          .returnsPromise();
+        convertReferral.resolves(mockReferral);
+
+        res.on('end', function() {
+          expect(res.statusCode).to.equal(200);
+          expect(res.statusMessage).to.equal('OK');
+          const data = res._getData();
+          console.log(data);
+          // console.log('### DATA ###', data);
+          expect(data.recipient).to.be.ok;
+          expect(data.sender).to.be.ok;
+          expect(data.recipient.email).to.equal(req.body.email);
+          expect(data.sender.referralLink).to.equal(req.body.referralLink);
+          expect(data.sender.amount_to_credit).to.equal(mockReferral.sender.amount_to_credit);
         });
 
-        response.on('error', function(err){
-          console.log('Error: ', err);
-        });
-
-        //stub marketing create
-        creditsRouter.handleSignups(request, response);
-        expect(response.statusCode).to.equal(200);
+        creditsRouter.handleSignups(req, res);
+        done();
       });
 
     });
