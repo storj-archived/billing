dd5d5348f4beb8b6ee02ba70629ad374e3bee670
dylanlott <lott.dylan@gmail.com>
2017-10-24 12:02:34 -0600

    setup payment processor route tests

---
+++ test/server/routes/payment-processors.unit.js
@@ -0,0 +1,38 @@
+const sinon = require('sinon');
+const stubPromise = require('sinon-stub-promise');
+const chai = require('chai');
+const expect = chai.expect;
+const assert = chai.assert;
+const Promise = require('bluebird');
+
+
+stubPromise(sinon);
+
+const PaymentProcessorsRouter = require('../../../lib/server/routes/payment-processors');
+const errors = require('storj-service-error-types');
+const routerOpts = require('../../_fixtures/router-opts');
+const constants = require('../../../lib/constants');
+const Config = require('../../../lib/config');
+
+describe('PaymentProcessors Router', () => {
+  const PaymentProc = new PaymentProcessorsRouter(routerOpts);
+  let sandbox;
+
+  beforeEach((done) => {
+    sandbox = sinon.sandbox.create();
+    done();
+  });
+
+  afterEach((done) => {
+    sandbox.restore();
+    done();
+  });
+
+  describe('@constructor', () => {
+    it('smoke test', (done) => {
+      expect(PaymentProc.config).to.be.instanceOf(Config);
+      expect(PaymentProc).to.be.instanceOf(PaymentProcessorsRouter);
+      done();
+    });
+  });
+});
