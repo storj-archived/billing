906df359a6a8f146be22ac58a406760ae6be7aa7
dylanlott <lott.dylan@gmail.com>
2017-12-05 11:26:36 -0700

    added another test to coinpayments

--- test/server/routes/coinpayments.unit.js
+++ test/server/routes/coinpayments.unit.js
@@ -147,79 +147,65 @@ describe('#coinpayments IPN router', function() {
 
       coinpayments.handleIPN(request, response);
     });
-//
-//     it('should create credit if one is not found', function (done) {
-//       sandbox.restore();
-//
-//       // const testRouter = new CoinpaymentsRouter(
-//       //   require('../../_fixtures/router-opts')
-//       // )
-//
-//       // function Credit (options) {
-//       //   expect(options).to.eql({
-//       //     paid: false,
-//       //     invoiced_amount: 100,
-//       //     paid_amount: 0,
-//       //     type: 'automatic',
-//       //     user: testUser,
-//       //     payment_processor: 'COINPAYMENTS'
-//       //   })
-//       // }
-//
-//       // testRouter.storage = {
-//       //   models: {
-//       //     Credit: Credit
-//       //   }
-//       // }
-//
-//       // Credit.prototype.save = sandbox.stub().callsArgWith(0, null);
-//
-//       const mockBody = require('../../_fixtures/coinpayments_req');
-//       const request = httpMocks.createRequest({
-//         method: 'POST',
-//         url: '/coinpayments'
-//       });
-//
-//       request.body = mockBody({
-//         status: 100,
-//         currency: 'STORJ'
-//       });
-//
-//       const response = httpMocks.createResponse({
-//         req: request,
-//         eventEmitter: EventEmitter
-//       });
-//
-//       const mockPaymentProcessor = new coinpayments.storage.models.PaymentProcessor({
-//         name: 'COINPAYMENTS',
-//         user: 'dylan@storj.io',
-//         default: true,
-//         rawData: [{ address: '1234' }],
-//         created: Date.now()
-//       });
-//
-//       const _findOne = sandbox
-//         .stub(coinpayments.storage.models.PaymentProcessor, 'findOne')
-//         .returnsPromise();
-//
-//       _findOne.resolves(mockPaymentProcessor);
-//
-//       const _findOneCredit = sandbox
-//         .stub(coinpayments.storage.models.Credit, 'findOne')
-//         .returnsPromise();
-//
-//       _findOneCredit.resolves();
-//
-//       const _credit = sandbox
-//         .stub(coinpayments.storage.models, 'Credit')
-//
-//       response.on('end', function () {
-//         const data = response._getData();
-//         expect(response.statusCode).to.equal(201);
-//         done();
-//       });
-//
-//       coinpayments.handleIPN(request, response);
-//     });
+
+    it('should create credit if one is not found', function (done) {
+      const mockBody = require('../../_fixtures/coinpayments_req');
+      const request = httpMocks.createRequest({
+        method: 'POST',
+        url: '/coinpayments'
+      });
+
+      request.body = mockBody({
+        currency: 'STORJ',
+        status: '100',
+        amount: '1000'
+      });
+
+      request.user = testUser;
+
+      const response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+
+      const mockPaymentProcessor = new coinpayments.storage.models.PaymentProcessor({
+        name: 'COINPAYMENTS',
+        user: 'dylan@storj.io',
+        default: true,
+        created: Date.now()
+      });
+
+      const mockCredit = new coinpayments.storage.models.Credit({
+        invoiced_amount: 1000,
+        type: 'automatic',
+        user: testUser._id,
+        payment_processor: 'COINPAYMENTS'
+      });
+
+      const _findPaymentProc = sandbox
+        .stub(coinpayments.storage.models.PaymentProcessor, 'findOne')
+        .returnsPromise()
+
+      _findPaymentProc.resolves(mockPaymentProcessor);
+
+      const _findOne = sandbox
+        .stub(coinpayments.storage.models.Credit, 'findOne')
+        .returnsPromise();
+      _findOne.resolves(undefined);
+
+      const _save = sandbox
+        .stub(coinpayments.models.Credit.prototype, 'save')
+        .returnsPromise();
+
+      _save.resolves(mockCredit);
+
+      response.on('end', function () {
+        const data = response._getData();
+        expect(data).to.equal('Created');
+        done();
+      });
+
+      coinpayments.handleIPN(request, response);
+    });
   });
 });
