3f33306e063bb492fbaa572535362e5cf4251d42
Dylan Lott <lott.dylan@gmail.com>
2018-01-22 15:18:28 -0700

    add test for vendor/coinpayments

--- test/server/vendor/coinpayments.js
+++ test/server/vendor/coinpayments.js
@@ -1,13 +1,14 @@
 const sinon = require('sinon');
 const chai = require('chai');
 const expect = chai.expect;
-// const coinpayments = require('../../../lib/server/vendor/coinpayments');
+const coinpayments = require('../../../lib/server/vendor/coinpayments');
 
-// describe('#coinpayments', () => {
-//   it('should return coinpayments client', (done) => {
-//     expect(coinpayments).to.not.equal(undefined);
-//     expect(coinpayments).to.be.instanceOf(Object);
-//     expect(coinpayments.credentials).to.be.instanceOf(Object);
-//     done();
-//   });
-// });
+describe('#coinpayments', () => {
+  it('should return coinpayments client', (done) => {
+    const client = coinpayments();
+    expect(client).to.not.equal(undefined);
+    expect(client).to.be.instanceOf(Object);
+    expect(client.credentials).to.be.instanceOf(Object);
+    done();
+  });
+});
